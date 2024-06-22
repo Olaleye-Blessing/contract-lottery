19390586ea9f7a745c27d68fb86cfbe467e3dcd6
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-06-22 12:51:13 +0100

    Allow players to enter raffle draw

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,15 +8,33 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
+    error Raffle__NotEnoughEthSent();
     uint256 private immutable i_entranceFee;
+    // @dev interval to operate the raffle draw in seconds
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players;
 
-    constructor(uint256 _entranceFee) {
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 _entranceFee, uint256 _interval) {
         i_entranceFee = _entranceFee;
+        i_interval = _interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+        s_players.push(payable(msg.sender));
 
-    function pickWinner() public {}
+        emit EnteredRaffle(msg.sender);
+    }
+
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
